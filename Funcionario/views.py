744fc4e56ebb11bf90b40465da994d80b537619a
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-04-15 22:40:38 -0300

    branch ok!

--- Funcionario/views.py
+++ Funcionario/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rota acessada: Funcionários, teste return to maine!")
+    return HttpResponse("Rota acessada: Funcionários, teste sem maine")
