6ea614d9c54daf2f3cc8fad92c8dd2733ad4d7a5
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-04-15 21:53:34 -0300

    Animal: fix fields and migrate

--- Animal/models.py
+++ Animal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Animal(models.Model):
+    nome_animal = models.CharField(max_length=300, null=False)
+    idade_animal = models.PositiveIntegerField(null=False)
+    tipo_animal = models.CharField(max_length=300, null=False)
+    id_baia_pertence = models.PositiveIntegerField()
+    peso_animal = models.DecimalField(max_digits=5,decimal_places=2)
+    obs = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome_animal + " - " + self.tipo_animal
