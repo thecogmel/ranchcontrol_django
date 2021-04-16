764d947c7bc66f89b5d1782626f16d9f65f5dfcd
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-04-15 21:53:16 -0300

    Baias: fix models

--- Baias/models.py
+++ Baias/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Baias(models.Model):
+    nome_baia = models.CharField(max_length=300, null=False)
+    capacidade = models.PositiveIntegerField(null=False)
+    definicao = models.TextField()
+    id_funcionario_responsavel = models.PositiveIntegerField()
+    nome_funcionario = models.CharField(max_length=300, null=False)
+
+    def __str__(self):
+        return self.nome_baia
