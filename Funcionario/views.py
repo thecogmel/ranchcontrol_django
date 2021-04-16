37a145ed235eb863f782301a692426f67b459746
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-04-15 22:34:40 -0300

    initial merged

--- Funcionario/views.py
+++ Funcionario/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rota acessada: Funcionários, teste!")
+    return HttpResponse("Rota acessada: Funcionários, teste return to maine!")
