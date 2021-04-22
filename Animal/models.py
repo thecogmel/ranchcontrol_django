d3cdc9ecf19a2a5eba3141f76ea26f7804ffacff
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2021-04-22 18:26:03 -0300

    fix animal field funcionario true

--- Animal/models.py
+++ Animal/models.py
@@ -8,7 +8,7 @@ class Animal(models.Model):
     idade_animal = models.PositiveIntegerField(null=False)
     tipo_animal = models.CharField(max_length=300, null=False)
     peso_animal = models.DecimalField(max_digits=5,decimal_places=2)
-    baia = models.ForeignKey("Baias.Baias", on_delete=models.CASCADE)
+    baia = models.ForeignKey("Baias.Baias", on_delete=models.CASCADE, null=True)
     obs = models.TextField(null=True, blank=True)
 
     def __str__(self):
